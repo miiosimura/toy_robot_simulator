bc340c771f2f80d2fdb6a7f2b7a3078b15f0a509
miiosimura <mirian.iosimura@gmail.com>
2024-08-22 15:25:00 -0300

    fix: adjust report controller + add remove robot button

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,16 +4,22 @@ class ReportsController < ApplicationController
   def index; end
 
   def place
-    redirect_to action: :index, row: params[:row], column: params[:column], direction: params[:direction]
+    redirect_with_params(params[:row], params[:column], params[:direction])
   end
 
   def move
     row, column = Report::Move.call(params[:row].to_i, params[:column].to_i, params[:direction])
-    redirect_to action: :index, row: row, column: column, direction: params[:direction]
+    redirect_with_params(row, column, params[:direction])
   end
 
   def change_direction
     direction = Report::Rotate.call(params[:command], params[:direction])
-    redirect_to action: :index, row: params[:row], column: params[:column], direction: direction
+    redirect_with_params(params[:row], params[:column], direction)
+  end
+
+  private
+
+  def redirect_with_params(row, column, direction)
+    redirect_to action: :index, row: row, column: column, direction: direction
   end
 end
